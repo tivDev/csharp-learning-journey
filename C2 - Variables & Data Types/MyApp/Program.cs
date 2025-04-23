d22ecd9cb33a292a655ef690cee598f1223436d5
Tiv <nguot.tiv@103ditech.com>
2025-04-23 13:49:17 +0700

    update

---
+++ C2 - Variables & Data Types/MyApp/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Variables & Data Types
+
+// bool
+bool isTrue = true;
+Console.WriteLine(isTrue);
+
+// char
+char letter = 'a';
+Console.WriteLine(letter.GetType());
+Console.WriteLine(letter);
+
+// string
+string name = "John";
+Console.WriteLine(name);
+
+// int
+int number = 42;
+Console.WriteLine(number);
+
+// float
+float pi = 3.14f;
+Console.WriteLine(pi);
+
+// double
+double bigNumber = 3.14159265359;
+Console.WriteLine(bigNumber);
+
+// decimal
+decimal money = 12.99m;
+Console.WriteLine(money);
+
---
+++ C3-LOOP/MyApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // Call the Add method and print the result
+            ForLoopExample();
+            WhileLoopExample();
+            DoWhileLoopExample();
+            ForEachLoopExample();
+        }
+        // 1.Method For Loop
+        static void ForLoopExample()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        // 2.Method While Loop
+        static void WhileLoopExample()
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+        // 3.Method Do While Loop
+        static void DoWhileLoopExample()
+        {
+            int i = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i < 10);
+        }
+        // 4.Method For Each Loop
+        static void ForEachLoopExample()
+        {
+            string[] names = { "Alice", "Bob", "Charlie" };
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
+
